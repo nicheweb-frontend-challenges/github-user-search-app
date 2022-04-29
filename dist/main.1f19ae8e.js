0425e86913df110fba7798e10bf76b0558a560a5
nicheweb <norgerojascerulia@gmail.com>
2022-04-29 21:30:47 +0200

    Fix the no result message margin bug

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -301,7 +301,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56759" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53755" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
